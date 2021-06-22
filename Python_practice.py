e454d66bef56f82eb0f64e123350153dc9ec3e66
Justin Christian Lee <justinclee24@gmail.com>
2021-06-22 15:11:40 -0600

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,45 @@
+print("Hello World")
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+
+# What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.')
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+
+for county in counties:
+    print(county)
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict:
+    print(county)
+for voters in counties_dict.values():
+    print(voters)
+for county, voters in counties_dict.items():
+    print(county, voters)
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
